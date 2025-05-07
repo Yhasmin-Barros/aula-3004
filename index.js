3507eff3a1baf9001f86d3370f1645deab2f6287
Yhasmin-Barros <yhasminbarros1@gmail.com>
2025-05-07 17:49:54 +0000

    Aula 07/05/2025

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ app.set('views', './views')
 app.use(express.static('public'))
 
 app.get('/', (request, response) => {
-    response.send('Testando!!!');
-    response.render('home');
+    //response.send('Testando!!!');
+    response.render('home', {nome : "Yhasmin"});
+});
+
+app.get('/tabuada', (request, response) => {
+    let resultado = [];
+    for (let cont = 1; cont<11; cont++){
+        resultado.push(cont * 5);
+    }
+    response.render('tabuada', {valores : resultado});
 });
 
 app.listen(3000, () => {
