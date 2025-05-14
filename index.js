354ae78c959cbeed12d9c7ee02a47c3a0a7dd8b4
Yhasmin-Barros <yhasminbarros1@gmail.com>
2025-05-14 17:47:15 +0000

    Express Template - 1405

--- index.js
+++ index.js
@@ -12,12 +12,18 @@ app.get('/', (request, response) => {
     response.render('home', {nome : "Yhasmin"});
 });
 
-app.get('/tabuada', (request, response) => {
+
+app.get('/tabuada/:numero', (request, response) => {
     let resultado = [];
+    const numero = request.params ['numero']; //params = número do parametro, requisição(request) recebe "numero", "/" é o parametro
+    //  que recebe//
+
     for (let cont = 1; cont<11; cont++){
-        resultado.push(cont * 5);
+        //resultado.push(cont * 5);
+        resultado.push(cont * numero);
     }
-    response.render('tabuada', {valores : resultado});
+    response.render('tabuada', {valores : resultado}); //valores: -cada propriedade que eu passar para ele vira valores//
+    //npm install é para garantir que está tudo ok//
 });
 
 app.listen(3000, () => {
